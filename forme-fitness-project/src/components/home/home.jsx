b90f603f5511d1128a851b5c0ed7bf85077aa02f
Cayo Santana <cc.santana.js@gmail.com>
2024-01-05 15:28:01 -0300

    Create homepage
    Done!

---
+++ forme-fitness-project/src/components/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css'
+
+const Home = () => {
+    return ( 
+        <>
+            <div className='container-home'>
+                <h1 className='title-home'>HOME</h1>
+            </div>
+        </>
+     );
+}
+ 
+export default Home;
